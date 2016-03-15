da0e7dcfe431c2432204bdb19e5ae4e7c8aaf82b
Larissa Rabello <lari.rabello@gmail.com>
2016-03-15 14:07:47 -0300

    exercise 12 was made

---
+++ exerc12.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class HelloWorld {
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    String x;
+    float n1, n2, oper=0;
+
+    System.out.println("Insira um simbolo: ");
+    x = s.next();
+
+    System.out.println("Insira um numero: ");
+    n1 = s.nextFloat();
+
+    System.out.println("Insira outro numero: ");
+    n2 = s.nextFloat();
+
+    if (x.equalsIgnoreCase("+")) {
+      oper=n1+n2;
+      System.out.println("A soma eh: "+oper);
+    }
+    if (x.equalsIgnoreCase("-")) {
+      oper=n1-n2;
+      System.out.println("A subtracao eh: "+oper);
+    }
+    if (x.equalsIgnoreCase("*")) {
+      oper=n1*n2;
+      System.out.println("A multiplicacao eh: "+oper);
+    }
+    if (x.equalsIgnoreCase("/")) {
+      oper=n1/n2;
+      System.out.println("A divisao eh: "+oper);
+    }
+
+  }
+
+}
