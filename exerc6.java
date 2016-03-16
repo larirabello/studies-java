e292e1c8faa54d09fcf770ee53195268250bd909
Larissa Rabello <lari.rabello@gmail.com>
2016-03-16 13:54:01 -0300

    exercise 6 is working now

--- exerc6.java
+++ exerc6.java
@@ -5,7 +5,7 @@ public class HelloWorld{
 		Scanner s = new Scanner(System.in);
     int idade=0, j=0, i=0, soma=0;
 
-    soma+=idade;
+
     while (soma<=99) {
       System.out.println("Insira a idade: ");
 		  idade = s.nextInt();
@@ -16,6 +16,7 @@ public class HelloWorld{
       if (idade>50) {
         i++;
       }
+      soma+=idade;
     }
 
     System.out.println("Pessoas menores de 21 anos: "+j);
