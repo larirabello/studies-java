f99cb9ce0280c1184c14e3e134a21ad823881b81
Larissa Rabello <lari.rabello@gmail.com>
2016-03-16 00:52:27 -0300

    exercise 12 was optimized using switch case

---
+++ exerc12switchcase.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+public class HelloWorld {
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    String x;
+    float n1, n2, oper=0;
+
+    System.out.println("Insira um simbolo: ");
+    x = s.next();
+
+    System.out.println("Insira um numero: ");
+    n1 = s.nextFloat();
+
+    System.out.println("Insira outro numero: ");
+    n2 = s.nextFloat();
+
+   switch (x) {
+        case "+":
+            oper=n1+n2;
+            System.out.println("A soma eh: "+oper);
+            break;
+        case "-":
+            oper=n1-n2;
+            System.out.println("A subtracao eh: "+oper);
+            break;
+        case "*":
+             oper=n1*n2;
+             System.out.println("A multiplicacao eh: "+oper);
+             break;
+        case "/":
+            oper=n1/n2;
+            System.out.println("A divisao eh: "+oper);
+            break;
+        case "abstruso":
+            System.out.println ("abstruse"); break;
+        }
+
+  }
+
+}
