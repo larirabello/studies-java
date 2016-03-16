e902c7b524cf06aaa86eb400b38eb0da5a805ae6
Larissa Rabello <lari.rabello@gmail.com>
2016-03-16 11:26:11 -0300

    exercise 6 still incomplete

---
+++ exerc6.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class HelloWorld{
+	public static void main(String[] args){
+    System.out.println("Idade");
+		Scanner s = new Scanner(System.in);
+    int idade;
+
+
+
+		System.out.println("Insert an age: ");
+		int idade = s.nextInt();
+
+    if ()
+	}
+}
+
+
+//incomplete
