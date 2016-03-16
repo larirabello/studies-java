7395351594c82d1177d29163f0e1f9c72d10b76b
Larissa Rabello <lari.rabello@gmail.com>
2016-03-16 13:24:46 -0300

    exercise 6 still not working

--- exerc6.java
+++ exerc6.java
@@ -1,18 +1,24 @@
 import java.util.Scanner;
 public class HelloWorld{
 	public static void main(String[] args){
-    System.out.println("Idade");
+    System.out.println("Idades");
 		Scanner s = new Scanner(System.in);
-    int idade;
+    int idade=0, j=0, i=0, soma=0;
 
+    soma+=idade;
+    while (soma<=99) {
+      System.out.println("Insira a idade: ");
+		  idade = s.nextInt();
 
+		  if (idade<21) {
+        j++;
+      }
+      if (idade>50) {
+        i++;
+      }
+    }
 
-		System.out.println("Insert an age: ");
-		int idade = s.nextInt();
-
-    if ()
+    System.out.println("Pessoas menores de 21 anos: "+j);
+    System.out.println("Pessoas com mais de 50 anos: "+i);
 	}
 }
-
-
-//incomplete
