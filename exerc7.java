7974a8f0b52abf95b186385bba11c2fb2c132e91
Larissa Rabello <lari.rabello@gmail.com>
2016-03-17 01:25:24 -0300

    exercise 7 was made

---
+++ exerc7.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Calculo horario");
+    Scanner s = new Scanner(System.in);
+    int sec=0, m=0, h=0;
+
+    System.out.println("Insira os segundos: ");
+    sec = s.nextInt();
+
+    m = sec/60;
+    sec = sec%60;
+
+    h = m/60;
+    m = m%60;
+
+    System.out.println(h+" hora "+m+" minutos e "+sec+" segundos");
+
+  }
+}
