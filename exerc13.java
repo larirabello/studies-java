accf3caef2ae1b067ce0b1570f41483818219ef2
Larissa Rabello <lari.rabello@gmail.com>
2016-03-17 11:44:47 -0300

    exercise 13 was made

---
+++ exerc13.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class HelloWorld {
+  public static void main(String[] args) {
+  Scanner s = new Scanner(System.in);
+  float horas=0, valor=0, sal=0;
+
+  System.out.println("Insira a quantidade de horas: ");
+  horas = s.nextFloat();
+  System.out.println("Insira o valor correspondente a hora: ");
+  valor = s.nextFloat();
+
+  if (horas<40) {
+    sal = horas*valor;
+    System.out.println("O salario mensal eh de "+sal);
+  } else if (horas>60) {
+    sal = (horas*valor)*2;
+    System.out.println("O salario mensal eh de "+sal);
+  } else {
+    sal = (horas*valor)*1.5f;
+    System.out.println("O salario mensal eh de "+sal);
+  }
+  }
+}
