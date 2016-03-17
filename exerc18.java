1bb7208e4c0eef0461183817d0d95cd62229ae9d
Larissa Rabello <lari.rabello@gmail.com>
2016-03-17 12:53:26 -0300

    now has constant

--- exerc18.java
+++ exerc18.java
@@ -1,13 +1,15 @@
 import java.util.Scanner;
 public class HelloWorld {
+  public static final float M = 700;
   public static void main(String[] args) {
     Scanner s = new Scanner(System.in);
+
     float fo=0, ac=0;
 
     System.out.println("Insira a aceleracao em m/s: ");
     ac = s.nextFloat();
 
-    fo = 700*ac;
+    fo = M*ac;
 
     System.out.println("A forca exercida eh de: "+fo+" N.");
   }
