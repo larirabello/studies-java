92827cf11485df569c570689fcc057b892a715a4
Larissa Rabello <lari.rabello@gmail.com>
2016-03-18 00:32:55 -0300

    first exercise from USP list was made

---
+++ ex1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Quadrados");
+    Scanner s = new Scanner(System.in);
+    int n1=0, quad=0;
+
+    System.out.println("Digite o primeiro parametro do intervalo: ");
+    n1 = s.nextInt();
+
+
+    for (int i=0; i<=n1; i++) {
+      quad=i*i;
+      System.out.println(quad);
+    }
+
+  }
+}
