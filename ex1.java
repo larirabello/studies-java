17587c3ed2d902ab1b674aa9fe73f3a729d88e25
Larissa Rabello <lari.rabello@gmail.com>
2016-03-18 14:21:06 -0300

    exercise 1 was modified and ex 2 was made - both from USP list

--- ex1.java
+++ ex1.java
@@ -4,16 +4,19 @@ public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Quadrados");
     Scanner s = new Scanner(System.in);
-    int n1=0, quad=0;
+    int n=0, quad=0;
 
     System.out.println("Digite o parametro do intervalo: ");
-    n1 = s.nextInt();
+    n = s.nextInt();
 
 
-    for (int i=0; i<=n1; i++) {
-      quad=i*i;
-      System.out.println(quad);
+    while (n != 0) {
+      quad = n*n;
+      System.out.println("O quadrado do valor eh: "+quad)
     }
 
   }
 }
+
+
+//weird
---
+++ ex2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Soma");
+    Scanner s = new Scanner(System.in);
+    int n=0, soma=0;
+
+    System.out.println("Digite um numero: ");
+    n = s.nextInt();
+
+    for (int i=0; i<=n; i++) {
+      soma += i;
+    }
+    System.out.println("A soma eh: "+soma);
+  }
+}
