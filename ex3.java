da2e20cecead14851ddc0fbfb6eb555d26742722
Larissa Rabello <lari.rabello@gmail.com>
2016-03-19 00:44:44 -0300

    exercise 3 was made

---
+++ ex3.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Soma");
+    Scanner s = new Scanner(System.in);
+    int n=0;
+
+    System.out.println("Digite um numero: ");
+    n = s.nextInt();
+
+    for (int i=0; i<=n; i++) {
+      if (i%2!=0) {
+        System.out.println("E' impar: "+i);
+      }
+    }
+
+  }
+}
