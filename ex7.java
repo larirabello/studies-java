9af8d6d2b0d9e34bdd67c4447434464aac2669ef
Larissa Rabello <lari.rabello@gmail.com>
2016-03-20 14:16:08 -0300

    exercise 7 from USP list was made

--- ex7.java
+++ ex7.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Impar");
+    System.out.println("Pares");
     Scanner s = new Scanner(System.in);
     int n=0;
 
@@ -10,8 +10,8 @@ public class HelloWorld {
     n = s.nextInt();
 
     for (int i=0; i<=n; i++) {
-      if (i%2!=0) {
-        System.out.println("E' impar: "+i);
+      if (i%2==0) {
+        System.out.println("E' par: "+i);
       }
     }
 
