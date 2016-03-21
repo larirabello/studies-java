675e1a8ce03db924bd577d6a16af0cab5cd7adb3
Larissa Rabello <lari.rabello@gmail.com>
2016-03-20 22:42:52 -0300

    ex 4 from USP list - power

---
+++ ex4.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Potenciacao");
+    Scanner s = new Scanner(System.in);
+    int x=0, n=0, p=0;
+
+    System.out.println("Digite a base: ");
+    x = s.nextInt();
+    System.out.println("Digite o expoente: ");
+    n = s.nextInt();
+
+    p = (int) Math.pow (x, n);
+
+    System.out.println("O resultado e': "+p);
+
+  }
+}
