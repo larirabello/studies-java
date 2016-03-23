03564da9331bebbe7bfa20a872087cb01f9f1894
Larissa Rabello <lari.rabello@gmail.com>
2016-03-23 17:23:16 -0300

    exercise 7 was finished.

--- ex7.java
+++ ex7.java
@@ -1,11 +1,10 @@
-/* Dados n e uma seqüência de n números inteiros, determinar a soma dos números pares.  */
 import java.util.Scanner;
 
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Pares");
     Scanner s = new Scanner(System.in);
-    int n=0;
+    int n=0, t=0;
 
     System.out.println("Digite um numero: ");
     n = s.nextInt();
@@ -13,8 +12,10 @@ public class HelloWorld {
     for (int i=0; i<=n; i++) {
       if (i%2==0) {
         System.out.println("E' par: "+i);
+        t+=i;
       }
-    }
 
+    }
+     System.out.println("\nTOTAL: "+t);
   }
 }
